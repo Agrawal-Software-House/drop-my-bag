89a734432994cb3738e1fe00afcf9e79f113b23f
Rsagrawals <ravi@reviewthatplae.com>
2021-07-25 17:25:20 +0530

    bug fixes

--- resources/views/merchant/auth/passwords/email.blade.php
+++ resources/views/merchant/auth/passwords/email.blade.php
@@ -2,46 +2,47 @@
 
 <!-- Main Content -->
 @section('content')
-<div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+<div class="container-fluid px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
+  <div class="card card0 border-0">
+      <div class="row d-flex">
+          <div class="col-lg-6">
+              <div class="card1 pb-5">
+                  <div class="row"> <img src="/logo/dmbF4.png" class="logo" style="height: 80px; width: 80px;"> </div>
+                  <div class="row px-3 justify-content-center mt-4 mb-5 border-line"> <img src="/merchant/login/img/merchant.jpg" class="img-fluid"> </div>
+              </div>
+          </div>
+          <div class="col-lg-6">
+              <form role="form" method="POST" action="{{ url('/seller/password/email') }}">
+                    {{ csrf_field() }}
+                  <div class="card2 card border-0 px-4 py-5">
 
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/merchant/password/email') }}">
-                        {{ csrf_field() }}
+                      <div class="row p-3"> 
+                        <label class="mb-1">
+                            <h6 class="mb-0 text-sm">Email Address</h6>
+                        </label> 
+                        <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" autofocus placeholder="Enter a valid email address"> 
+                        @if ($errors->has('email'))
+                            <span class="text-danger">
+                                <strong>{{ $errors->first('email') }}</strong>
+                            </span>
+                        @endif
+                      </div>
 
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                      <div class="row mb-3 p-2"> 
+                        <button type="submit" class="btn btn-blue text-center">Send Password Reset Link</button> 
+                      </div>
+                      
+                  </div>
+              </form>
+          </div>
+      </div>
+      <div class="bg-blue py-4">
+          <div class="row px-3"> <small class="ml-4 ml-sm-5 mb-2">Copyright &copy; 2021. All rights reserved.</small>
+              <div class="social-contact ml-4 ml-sm-auto"> <span class="fa fa-facebook mr-4 text-sm"></span> <span class="fa fa-google-plus mr-4 text-sm"></span> <span class="fa fa-linkedin mr-4 text-sm"></span> <span class="fa fa-twitter mr-4 mr-sm-5 text-sm"></span> </div>
+          </div>
+      </div>
+  </div>
+</div> 
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}">
 
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    Send Password Reset Link
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
 @endsection
