e04914befaaf7ab8df00cb4d3f9bf7cf0632a86a
DESKTOP-IKD2D8I\Administrator <agrawalravi.1110@gmail.com>
2021-12-11 15:31:48 +0530

    bug fix

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -25,7 +25,7 @@ class ComposerAutoloaderInit91c4f566a07a3830e8350eaf0331c1d8
         require __DIR__ . '/platform_check.php';
 
         spl_autoload_register(array('ComposerAutoloaderInit91c4f566a07a3830e8350eaf0331c1d8', 'loadClassLoader'), true, true);
-        self::$loader = $loader = new \Composer\Autoload\ClassLoader();
+        self::$loader = $loader = new \Composer\Autoload\ClassLoader(\dirname(\dirname(__FILE__)));
         spl_autoload_unregister(array('ComposerAutoloaderInit91c4f566a07a3830e8350eaf0331c1d8', 'loadClassLoader'));
 
         $useStaticLoader = PHP_VERSION_ID >= 50600 && !defined('HHVM_VERSION') && (!function_exists('zend_loader_file_encoded') || !zend_loader_file_encoded());
